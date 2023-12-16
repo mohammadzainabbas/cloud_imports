0fb33afcd3140dcbc25f8ad1922856f3d07670f4
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-12-16 04:00:47 +0100

    🧹 Cleaned up unnecessary update

--- cloud_importer.py
+++ cloud_importer.py
@@ -1,5 +1,5 @@
 from sys import meta_path
 
-def add_repo(repo_url: str) -> None:
+def add_repo(repo_url: str | None) -> None:
     """Add a repository to sys.meta_path"""
     meta_path.append(CloudFinder(repo_url))
