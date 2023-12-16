ce1b0637a74211120981a671e71ef809c72480af
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-12-16 04:01:08 +0100

    🎭 Added support for some code

--- cloud_importer.py
+++ cloud_importer.py
@@ -1,5 +1,8 @@
 from sys import meta_path
+from .cloud_finder import CloudFinder
 
 def add_repo(repo_url: str | None) -> None:
-    """Add a repository to sys.meta_path"""
+    """
+    Add a repository to `sys.meta_path`
+    """
     meta_path.append(CloudFinder(repo_url))
