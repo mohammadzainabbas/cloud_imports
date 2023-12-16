924f2f54ddeff698c96372fc416a5d8acbbfe8ae
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-12-16 03:11:41 +0100

    🔧 Fixed minor bugs in the code

--- utils.py
+++ utils.py
@@ -1,4 +1,8 @@
 import ast
+imfrom typing import IO
+
+def is_valid_python_source(source: str | IO[str]) -> bool:
+    # rest of your code
 
 def is_valid_python_source(source: str | ReadableBuffer) -> bool:
     try:
