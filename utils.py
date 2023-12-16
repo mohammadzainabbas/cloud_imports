1f8aefa58cfa3e64d2fb0ec8d721474a90a3c06e
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-12-16 03:12:23 +0100

    🔒 Enhanced a feature

--- utils.py
+++ utils.py
@@ -1,9 +1,12 @@
 import ast
 from typing import IO
 
-def is_valid_python_source(source: str | IO[str]) -> bool:
+def is_valid_python_code(code: str | IO[str]) -> bool:
+    """
+    Check if the given code is valid Python code.
+    """
     try:
-        ast.parse(source)
+        ast.parse(code)
     except SyntaxError:
         return False
     return True
