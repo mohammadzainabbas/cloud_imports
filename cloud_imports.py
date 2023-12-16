bc140d12ce3bcecd70dd16b9f1052eee41c33b04
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-12-16 03:15:12 +0100

    🧱 Laid groundwork for some features

--- cloud_imports.py
+++ cloud_imports.py
@@ -12,12 +12,4 @@ class CloudFinder(importlib.abc.MetaPathFinder):
         self.base_url = base_url
     
     def find_spec(self, fullname, path, target=None):
-        if path is None:
-            path = ['']
-        if path[0] != '':
-            return None
-        logging.debug(f'find_spec({fullname!r}, {path!r}, {target!r})')
-        spec = self._find_spec(fullname, path)
-        logging.debug(f'find_spec({fullname!r}, {path!r}, {target!r}) -> {spec!r}')
-        return spec
     
