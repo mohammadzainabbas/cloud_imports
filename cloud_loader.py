8e3171046dd034a3b0e6889c25ca1101abf6c6f2
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-12-16 03:54:25 +0100

    🎉 Celebrated completing some modules

--- cloud_loader.py
+++ cloud_loader.py
@@ -10,13 +10,13 @@ class CloudLoader(Loader):
         self.source_code = source_code
         self.url = url
 
-    def create_module(self, spec: ModuleSpec | None = None) -> None:
+    def create_module(self, spec: ModuleSpec | None = None) -> ModuleType | None:
         module = sys.modules.get(spec.name)
         if module is None:
             module = ModuleType(spec.name)
             sys.modules[spec.name] = module
         return module
     
-    def exec_module(self, module: ModuleType) -> None:
-        
+    def exec_module(self, module: ModuleType) -> ModuleType:
+        module.__file__ = self.url
         exec(self.source_code, module.__dict__)
