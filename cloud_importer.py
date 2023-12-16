0b035594c75ae948042d03c3f31253c1941cea89
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-12-16 04:07:27 +0100

    🔧 Fixed minor issues in some improvements

--- cloud_importer.py
+++ cloud_importer.py
@@ -7,12 +7,6 @@ def add_repo(repo_url: str | None) -> None:
     """
     meta_path.append(CloudFinder(repo_url))
 
-def add_git_repo(repo_url: str | None) -> None:
-    """
-    Add a git repository to `sys.meta_path`
-    """
-    add_repo(repo_url)
-
 def add_github_repo(repo_url: str | None) -> None:
     """
     Add a github repository to `sys.meta_path`
