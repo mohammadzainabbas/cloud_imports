7bc0d857dfc8a2b25ae682a7a734380119cf7cc8
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-12-16 03:26:25 +0100

    📦 Packaged an improved code

--- cloud_imports.py
+++ cloud_imports.py
@@ -4,6 +4,7 @@ import importlib.machinery
 import sys
 import logging
 import requests
+from typing import Any
 
 from .utils import is_valid_python_code
 
@@ -11,7 +12,7 @@ class CloudFinder(importlib.abc.MetaPathFinder):
     def __init__(self, base_url: str):
         self.base_url = base_url
     
-    def find_spec(self, fullname: str, path: Any, target=None):
+    def find_spec(self, fullname: str | Any, path: Any, target=None):
         spec = self._find_py_file_spec(fullname)
         if spec is not None: return spec
         spec = self._find_package_init_spec(fullname)
