b508364c1309a9360a227cea6b73fe50d770d8ce
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-12-16 03:35:11 +0100

    🎭 Added support for a new feature

--- utils.py
+++ utils.py
@@ -12,3 +12,19 @@ def is_valid_python_code(code: str | IO[str]) -> bool:
     except SyntaxError:
         return False
     return True
+
+def get_remote_python_source(url: str) -> str | None:
+    """
+    Get the Python source code from the given URL.
+    """
+    try:
+        response = get(url)
+        response.raise_for_status()
+        source = response.text
+        if not is_valid_python_code(source):
+            raise SyntaxError("Not a valid Python code")
+        return source
+    except HTTPError:
+        return None
+    except SyntaxError:
+        return None
