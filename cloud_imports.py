0d26ee895654bdd59829469405468bb040cdc1f0
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-12-16 03:14:30 +0100

    🎭 Added support for some requirements

--- cloud_imports.py
+++ cloud_imports.py
@@ -10,3 +10,14 @@ from .utils import is_valid_python_code
 class CloudFinder(importlib.abc.MetaPathFinder):
     def __init__(self, base_url: str):
         self.base_url = base_url
+    
+    def find_spec(self, fullname, path, target=None):
+        if path is None:
+            path = ['']
+        if path[0] != '':
+            return None
+        logging.debug(f'find_spec({fullname!r}, {path!r}, {target!r})')
+        spec = self._find_spec(fullname, path)
+        logging.debug(f'find_spec({fullname!r}, {path!r}, {target!r}) -> {spec!r}')
+        return spec
+    
