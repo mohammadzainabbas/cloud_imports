efd965a0dc58edcab49994fa9ad76c01db0baab2
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-12-16 03:44:35 +0100

    🎭 Added support for a module

--- cloud_imports.py
+++ cloud_imports.py
@@ -5,10 +5,10 @@ import requests
 from typing import Any
 
 from .cloud_loader import CloudLoader
-from .utils import is_valid_python_code
+from .utils import get_remote_python_source
 
 class CloudFinder(MetaPathFinder):
-    
+
     def __init__(self, base_url: str) -> None:
         self.base_url = base_url
     
@@ -20,27 +20,15 @@ class CloudFinder(MetaPathFinder):
     
     def _find_py_file_spec(self, fullname: str):
         url = f"{self.base_url}/{fullname.replace(".", "/")}.py"
-        source = self._get_remote_python_source(url)
+        source = get_remote_python_source(url)
         if source is None: return None
         loader = CloudLoader(fullname, source, url)
         return ModuleSpec(fullname, loader, origin=url)
     
     def _find_package_init_spec(self, fullname: str):
         url = f"{self.base_url}/{fullname.replace('.', '/')}/__init__.py"
-        source = self._get_remote_python_source(url)
+        source = get_remote_python_source(url)
         if source is None: return None
         loader = CloudLoader(fullname, source, url)
         return ModuleSpec(fullname, loader, origin=url, is_package=True)
     
-    def _get_remote_python_source(self, url: str) -> str | None:
-        try:
-            response = requests.get(url)
-            response.raise_for_status()
-            source = response.text
-            if not is_valid_python_code(source):
-                raise SyntaxError("Not a valid Python code")
-            return source
-        except requests.exceptions.HTTPError:
-            return None
-        except SyntaxError:
-            return None
