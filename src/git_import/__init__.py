198fc969971b04629e898a7ae39e291e02f8aab6
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-12-17 09:22:55 +0100

    🕵🏻 Conducted code review for an improved code

--- src/git_import/__init__.py
+++ src/git_import/__init__.py
@@ -7,11 +7,12 @@ from .git_finder import CloudFinder
 from .git_importer import GithubRepo, add_repo, add_github_repo, extract_github_info
 
 __all__ = [
-    "get_remote_python_source",
-    "is_valid_python_code",
     "CloudLoader",
     "CloudFinder",
+    "GithubRepo",
     "add_repo",
     "add_github_repo",
     "extract_github_info",
+    "get_remote_python_source",
+    "is_valid_python_code",
 ]
