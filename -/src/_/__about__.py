8f2f1ac6826cf291770e618c403c66607f43ba2b
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-12-17 08:32:30 +0100

    📲 Enhanced an improvement

--- -/src/_/__about__.py
+++
@@ -1,4 +0,0 @@
-# SPDX-FileCopyrightText: 2023-present Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
-#
-# SPDX-License-Identifier: MIT
-__version__ = "0.0.1"
--- -/src/_/__main__.py
+++
@@ -1,9 +0,0 @@
-# SPDX-FileCopyrightText: 2023-present Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
-#
-# SPDX-License-Identifier: MIT
-import sys
-
-if __name__ == "__main__":
-    from _.cli import _
-
-    sys.exit(_())
