bf12a52c954ab108efc1e9a9d9695d280eee8582
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-12-17 09:32:17 +0100

    🐛 Fixed minor typos in a feature

--- tests/test_importer.py
+++ tests/test_importer.py
@@ -1,7 +1,8 @@
 from git_import.git_importer import GithubRepo, add_repo, add_github_repo, extract_github_info
 
 def main():
-    GithubRepo(username="mohammadzainabbas", repo="cloud_imports", branch="main")
+    gh_repo = GithubRepo(username="mohammadzainabbas", repo="cloud_imports", branch="main")
+    print(gh_repo)
 
 if __name__ == "__main__":
     main()
