167630c4a2c5ba0158b306e333fafda845c14df8
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-12-17 09:23:17 +0100

    ✅ Passed all tests for a module

--- src/git_import/__init__.py
+++ src/git_import/__init__.py
@@ -1,14 +1,14 @@
 # SPDX-FileCopyrightText: 2023-present Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
 #
 # SPDX-License-Identifier: MIT
-from .utils import get_remote_python_source, is_valid_python_code
-from .git_loader import CloudLoader
 from .git_finder import CloudFinder
+from .git_loader import CloudLoader
 from .git_importer import GithubRepo, add_repo, add_github_repo, extract_github_info
+from .utils import get_remote_python_source, is_valid_python_code
 
 __all__ = [
-    "CloudLoader",
     "CloudFinder",
+    "CloudLoader",
     "GithubRepo",
     "add_repo",
     "add_github_repo",
