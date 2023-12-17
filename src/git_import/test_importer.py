af3c56c8cb1b32f28d909c66905ade7ad78ffb4e
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-12-17 09:34:22 +0100

    🌍 Made some code

---
+++ src/git_import/test_importer.py
@@ -0,0 +1,8 @@
+from git_importer import GithubRepo, add_repo, add_github_repo, extract_github_info
+
+def main():
+    gh_repo = GithubRepo(username="mohammadzainabbas", repo="cloud_imports", branch="main")
+    print(gh_repo)
+
+if __name__ == "__main__":
+    main()
