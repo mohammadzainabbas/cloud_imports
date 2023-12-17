432bc1fb54316af99ecf16361ea67382eaba674a
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-12-17 10:12:32 +0100

    📈 Better workflow for some code

--- src/git_import/test_importer.py
+++ src/git_import/test_importer.py
@@ -3,15 +3,15 @@ from src.git_import.git_importer import GithubRepo, add_repo, add_github_repo, e
 def main():
     gh_repo = GithubRepo(username="mohammadzainabbas", repo="cloud_imports", branch="main")
     print(gh_repo)
-    
-    add_github_repo("https://github.com/kiranmantri/python-remote-import")
-    from remote_import import RemoteImporter
 
-    r = RemoteImporter.add_remote(
-        namespaces=["test_package"],
-        base_url ='github://kiranmantri:python-remote-import@/examples'
-        )
-    print(r)
+    # add_github_repo("https://github.com/kiranmantri/python-remote-import")
+    # from remote_import import RemoteImporter
+
+    # r = RemoteImporter.add_remote(
+    #     namespaces=["test_package"],
+    #     base_url ='github://kiranmantri:python-remote-import@/examples'
+    #     )
+    # print(r)
 
 if __name__ == "__main__":
     main()
