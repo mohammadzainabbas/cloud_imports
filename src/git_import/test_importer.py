45dd73efea719a8ad77546dca6593ffd4147bea9
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-12-17 09:36:28 +0100

    🧹 Cleaned up unnecessary requirement

--- src/git_import/test_importer.py
+++ src/git_import/test_importer.py
@@ -1,4 +1,4 @@
-from git_importer import GithubRepo, add_repo, add_github_repo, extract_github_info
+from src.git_import.git_importer import GithubRepo, add_repo, add_github_repo, extract_github_info
 
 def main():
     gh_repo = GithubRepo(username="mohammadzainabbas", repo="cloud_imports", branch="main")
