0cd2abdc66fe5bc5ec25e9e4379c9db405e38202
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-12-17 10:06:39 +0100

    🎉 Celebrated completing an improvement

--- tests/test_importer.py
+++ tests/test_importer.py
@@ -1,8 +1,17 @@
-from ..src.git_import.git_importer import GithubRepo, add_repo, add_github_repo, extract_github_info
+from src.git_import.git_importer import GithubRepo, add_repo, add_github_repo, extract_github_info
 
 def main():
     gh_repo = GithubRepo(username="mohammadzainabbas", repo="cloud_imports", branch="main")
     print(gh_repo)
+    
+    add_github_repo("https://github.com/kiranmantri/python-remote-import")
+    from remote_import import RemoteImporter
+
+    r = RemoteImporter.add_remote(
+        namespaces=["test_package"],
+        base_url ='github://kiranmantri:python-remote-import@/examples'
+        )
+    print(r)
 
 if __name__ == "__main__":
     main()
